bc8683597567952afa03c74ac66b450d45bd5b6a
DanielKiernan <DanielKiernanJr@gmail.com>
2020-12-28 23:01:05 -0600

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -14,45 +14,45 @@ function elt(name, attributes) {
   return node;
 }
 
-var controls = Object.create(null)
+var controls = Object.create(null);
 
 function createPaint(parent) {
-var canvas = elt("canvas", {width: 500, height: 300});
-var cx = canvas.getContext("2d");
-var toolbar = elt("div", {class: "toolbar"});
-for (var name in controls)
-  toolbar.appendChild(controls[name](cx));
+  var canvas = elt("canvas", {width: 500, height: 300});
+  var cx = canvas.getContext("2d");
+  var toolbar = elt("div", {class: "toolbar"});
+  for (var name in controls)
+    toolbar.appendChild(controls[name](cx));
 
-var panel = elt("div", {class: "picturepanel"});
-parent.appendChild(elt("div", null, panel, toolbar);
+  var panel = elt("div", {class: "picturepanel"}, canvas);
+  parent.appendChild(elt("div", null, panel, toolbar));
 }
 
 var tools = Object.create(null);
 
 controls.tool = function(cx) {
   var select = elt("select");
-  for var name in tools
+  for (var name in tools)
     select.appendChild(elt("option", null, name));
-    
-    cx.canvas.addEventListener("mousedown", function(Event) {
-      if (event.which == 1) {
+
+  cx.canvas.addEventListener("mousedown", function(event) {
+    if (event.which == 1) {
       tools[select.value](event, cx);
       event.preventDefault();
-      }
-    });
-  
+    }
+  });
+
   return elt("span", null, "Tool: ", select);
-  };
+};
 
 function relativePos(event, element) {
-  var rect = element.getBoundingClientRec();
+  var rect = element.getBoundingClientRect();
   return {x: Math.floor(event.clientX - rect.left),
           y: Math.floor(event.clientY - rect.top)};
 }
 
 function trackDrag(onMove, onEnd) {
   function end(event) {
-    removeEventListener("mouseMove", onMove);
+    removeEventListener("mousemove", onMove);
     removeEventListener("mouseup", end);
     if (onEnd)
       onEnd(event);
@@ -61,9 +61,9 @@ function trackDrag(onMove, onEnd) {
   addEventListener("mouseup", end);
 }
 
-tools.Line = function(event. cx. onEnd) {
+tools.Line = function(event, cx, onEnd) {
   cx.lineCap = "round";
-  
+
   var pos = relativePos(event, cx.canvas);
   trackDrag(function(event) {
     cx.beginPath();
@@ -80,3 +80,74 @@ tools.Erase = function(event, cx) {
     cx.globalCompositeOperation = "source-over";
   });
 };
+
+controls.brushSize = function(cx) {
+  var select = elt("select");
+  var sizes = [1, 2, 3, 5, 8, 12, 25, 35, 50, 75, 100];
+  sizes.forEach(function(size) {
+    select.appendChild(elt("option", {value: size},
+                           size + " pixels"));
+  });
+  select.addEventListener("change", function() {
+    cx.lineWidth = select.value;
+  });
+  return elt("span", null, "Brush size: ", select);
+};
+  
+controls.save = function(cx) {
+  var link = elt("a", {href: "/"}, "Save");
+  function update() {
+    try {
+      link.href = cx.canvas.toDataURL();
+    } catch (e) {
+      if (e instanceof SecurityError)
+        link.href = "javascript:alert(" +
+          JSON.stringify("Can't save: " + e.toString()) + ")";
+      else
+        throw e;
+    }
+  }
+  link.addEventListener("mouseover", update);
+  link.addEventListener("focus", update);
+  return link;
+};
+  
+function loadImageURL(cx, url) {
+  var image = document.createElement("img");
+  image.addEventListener("load", function() {
+    var color = cx.fillStyle, size = cx.lineWidth;
+    cx.canvas.width = image.width;
+    cx.canvas.height = image.height;
+    cx.drawImage(image, 0, 0);
+    cx.fillStyle = color;
+    cx.strokeStyle = color;
+    cx.lineWidth = size;
+  });
+  image.src = url;
+}
+  
+controls.openFile = function(cx) {
+  var input = elt("input", {type: "file"});
+  input.addEventListener("change", function() {
+    if (input.files.length == 0) return;
+    var reader = new FileReader();
+    reader.addEventListener("load", function() {
+      loadImageURL(cx, reader.result);
+    });
+    reader.readAsDataURL(input.files[0]);
+  });
+  return elt("div", null, "Open file: ", input);
+};
+  
+controls.openURL = function(cx) {
+  var input = elt("input", {type: "text"});
+  var form = elt("form", null,
+                 "Open URL: ", input,
+                 elt("button", {type: "submit"}, "load"));
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    loadImageURL(cx, input.value);
+  });
+  return form;
+};
+  
